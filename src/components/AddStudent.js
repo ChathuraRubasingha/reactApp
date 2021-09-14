d09efc1ab0ce6f125ee1d8b53f1a615298d7b465
ChathuraRubasingha <rubasingharcpk.19@uom.lk>
2021-09-14 15:07:48 +0530

    addStudent.jd is modified!

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,20 +1,46 @@
 import react, {useState} from "react";
 
 export default function Addstudent () {
+
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("");
+
+
+    function sendData (e) {
+        e.preventDefault();
+        const studentData = {
+            name,
+            age,
+            gender
+        }
+
+        console.log(studentData);
+    }
+
     return (
         <div className="container" style={{width:'30%', padding: '50px'}}>
-            <form>
+            <form onSubmit = {sendData}>
                 <div class="mb-3">
                     <label for="studentName" class="form-label">Student Name</label>
-                    <input type="text" class="form-control" id="studentName"></input>
+                    <input type="text" class="form-control" id="studentName" 
+                    onChange={(e)=>{
+                        setName(e.target.value);
+                    }}></input>
                 </div>
                 <div class="mb-3">
                     <label for="Age" class="form-label">Age</label>
-                    <input type="text" class="form-control" id="Age"></input>
+                    <input type="text" class="form-control" id="Age" 
+                    onChange={(e)=>{
+                        setAge(e.target.value);
+                    }}></input>
                 </div>
                 <div class="mb-3">
                     <label for="Gender" class="form-label">Gender</label>
-                    <input type="text" class="form-control" id="Gender"></input>
+                    <input type="text" class="form-control" id="Gender"
+                    onChange={(e)=>{
+                        setGender(e.target.value);
+                    }}></input>
                 </div>
                 <div class="mb-3 form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
